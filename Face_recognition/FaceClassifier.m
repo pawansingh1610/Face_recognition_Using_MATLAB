5cda785013ea7c4851382ff89efeb7cd96a1797f
pawansingh1610 <pawansingh1610@gmail.com>
2018-06-07 09:21:41 +0530

    Add files via upload

--- Face_recognition/FaceClassifier.m
+++ Face_recognition/FaceClassifier.m
@@ -14,14 +14,23 @@ load db.mat
 Ftrain=db(:,1:2);
 Ctrain=db(:,3);
 for i=1:size(Ftrain,1)
-dist(i,:)=sum(abs(Ftrain(i,:)-Ftest));
+dist(i,:)=sum(abs(str2double(Ftrain(i,:)))-Ftest);
 end
 Min=min(dist);
 if (Min<3)
 m=find(dist==Min,1);
 det_class = Ctrain(m);
-msgbox(strcat('detected class=',num2str(det_class)));
-else
-    msgbox('This person is not registered');
+switch (det_class)
+          case {"1"}
+           msgbox(strcat('detected Person=','Pawan'));
+          case {"2"}
+            msgbox(strcat('detected Person=','Mrinal'));
+          case {"3"}
+           msgbox(strcat('detected Person=','Hj'));
+          case {"4"}
+           msgbox(strcat('detected Person=','Ishant'));
+    otherwise
+            disp('Unknown Person')
 end
-    
+
+ end
